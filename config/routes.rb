f24a59148e16252dea342bfc339a1e7bde463626
Francisco <fazpandy@icloud.com>
2020-03-08 15:05:02 -0300

    ajustes navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :events
-  get 'events/index'
+  {
+     sessions: 'users/sessions',
+     registrations: 'users/registrations',
+     delete: 'sessions/destroy'
+   }
+  get 'event/index'
   root to: "events#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
